41a4c629c9fc634c06433c74b20f79412872edd4
dayoung-lee <dimmaculatey@gmail.com>
2020-06-26 19:23:31 +0900

    Updates

---
+++ precache-manifest.1bbf3eabe4f3acff07e31f8747f7d5a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d13ace3bf7b5435fcd878009b742d247",
+    "url": "/book_club/index.html"
+  },
+  {
+    "revision": "c01ff1990735a6e1cec3",
+    "url": "/book_club/static/css/main.71c16e09.chunk.css"
+  },
+  {
+    "revision": "9d38c30f04c9eb0dd5e1",
+    "url": "/book_club/static/js/2.fc0d8fc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/book_club/static/js/2.fc0d8fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01ff1990735a6e1cec3",
+    "url": "/book_club/static/js/main.1b936410.chunk.js"
+  },
+  {
+    "revision": "6d5d3fe0895cbd6c2616",
+    "url": "/book_club/static/js/runtime-main.c82ee41e.js"
+  }
+]);
