141343590872a8063227a67cddfef4e8accd0106
dayoung-lee <dimmaculatey@gmail.com>
2020-06-30 11:53:57 +0900

    Home component modify

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ import "./home.css";
 function Home () {
   return (
     <div className="inner_wrap">
-      <h1 className="home_title">읽는다</h1>
-      <button className="btn_login">로그인</button>
-      <a href="/" className="signup">회원가입</a>
+        <h1 className="home_title">읽<br/>는<br/>다.</h1>
+        <button className="btn_login">로그인</button>
+        <a href="/" className="signup">회원이 아니신가요? 회원가입</a>
     </div>
   )
 }
